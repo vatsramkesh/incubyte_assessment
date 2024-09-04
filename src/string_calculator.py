2649a37b1fcfe8c504a360b0c8f8c7f730fb56ba
ramkesh <ramkeshvats@gmail.com>
2024-09-04 23:34:30 +0530

    Code to handle negative number

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,19 +1,40 @@
+from src.exceptions import InvalidInputException
 
 
 def add(input_string: str) -> int:
+    """
+    :param input_string: string of numbers to be added
+    :return: Calculated the sum of input number
+    """
     # If the input string is empty, return 0
     result = 0
     if not input_string:
         return result
-    # Replacing \n with comma
-    input_string = input_string.replace("\n", ',')
 
+    if input_string.startswith("//"):
+        delimiter_end_index = input_string.find("\n")
+        delimiter = input_string[2:delimiter_end_index]
+        input_string = input_string[delimiter_end_index + 1 :]
+
+        # Replace all instances of the delimiter with commas
+        input_string = input_string.replace(delimiter, ",")
+
+    # Replace \n with comma
+    input_string = input_string.replace("\n", ",")
+
+    negative_numbers = []
     # Split the input string by commas and iterate over splitted list and calculate sum
-    for num in input_string.split(','):
+    for num in input_string.split(","):
         # If condition to handle , at the end of input string
         if num:
-            # Since input is a string, so converting individual element to int
-            result += int(num)
-    
-    return result
+            if int(num) < 0:
+                negative_numbers.append(num)
+            else:
+                # Since input is a string, so converting individual element to int
+                result += int(num)
 
+    if negative_numbers:
+        raise InvalidInputException(
+            f"negative numbers not allowed {', '.join(negative_numbers)}"
+        )
+    return result
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -3,10 +3,11 @@ Test cases to test string calculator
 """
 import unittest
 
+from src.exceptions import InvalidInputException
 from src.string_calculator import add
 
-class TestStringCalculator(unittest.TestCase):
 
+class TestStringCalculator(unittest.TestCase):
     def test_add_with_empty_string(self):
         result = add("")
         self.assertEqual(result, 0)
@@ -26,3 +27,10 @@ class TestStringCalculator(unittest.TestCase):
     def test_add_with_new_line_between_and_end_of_numbers(self):
         result = add("1\n2,3, 6\n")
         self.assertEqual(result, 12)
+
+    def test_add_with_custom_delimeter(self):
+        result = add("//;\n1;2;3")
+        self.assertEqual(result, 6)
+
+    def test_add_with_negative_numbers(self):
+        self.assertRaises(InvalidInputException, add, "1,-2,3,-7")
